09634160446e832c05a477ea83ee172a90834d6d
shivamdangi7 <shivamdangi0111@gmail.com>
2024-10-22 22:48:26 +0530

    connecting db and creating a model

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express= require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/camp-trails' )
+
+const db = mongoose.connection;
+db.on("error" , console.error.bind(console, "connection error:"))
+db.once("open" , () =>{
+    console.log("Database connected");
+});
+
+const app = express();
 app.set('view engine' , 'ejs');
 app.set('views' , path.join(__dirname, 'views'))
 

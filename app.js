52f0fd108949c2918dddbfe1fd5f2e26668748e0
shivamdangi7 <shivamdangi0111@gmail.com>
2024-10-23 21:35:08 +0530

    catching Express errors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -39,6 +40,7 @@ app.get('/campgrounds/new' , (req,res) =>{
 })
 
 app.post('/campgrounds' , catchAsync(async(req,res,next) =>{
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data' , 400)
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -66,8 +68,14 @@ app.delete('/campgrounds/:id' ,catchAsync( async(req,res) =>{
     res.redirect('/campgrounds')
 }))
 
+app.all('*' , (req,res,next)=>{
+    next(new ExpressError('Page Not Found' , 404))
+})
+
 app.use((err,req,res,next)=>{
-    res.send('OH boy, SOMETHING WENT WRONG!!')
+    const {statusCode=500 , message='Something Went Wrong'} = err;
+    res.status(statusCode).send(message)
+    // this type of error only works with async errors
 })
 
 app.listen('3000' , ()=>{

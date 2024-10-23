4a6be1dbb87c2af150bc19a60c10e7e06b1629ba
shivamdangi7 <shivamdangi0111@gmail.com>
2024-10-23 09:49:52 +0530

    added the new campground route and form

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ db.once("open" , () =>{
 const app = express();
 app.set('view engine' , 'ejs');
 app.set('views' , path.join(__dirname, 'views'))
+// now the req.body is parsed
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/' , (req,res)=>{
     res.render('home')
@@ -23,6 +25,23 @@ app.get('/campgrounds' , async (req, res)=>{
     const campgrounds= await Campground.find({});
     res.render('campgrounds/index' , {campgrounds})
 })
+
+// order matter here , if otherwise new will be treated as id , and page doesn't load.
+app.get('/campgrounds/new' , (req,res) =>{
+    res.render('campgrounds/new' )
+})
+
+app.get('/campgrounds/:id' , async (req,res)=>{
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show' , { campground });
+})
+
+app.post('/campgrounds' , async(req,res) =>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.listen('3000' , ()=>{
     console.log("Serving on port 3000")
 });

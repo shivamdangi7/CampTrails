a267bd25d5567d8eda89ac4b35825cd06c3e781b
shivamdangi7 <shivamdangi0111@gmail.com>
2024-10-23 19:39:38 +0530

    added basic styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -13,6 +14,8 @@ db.once("open" , () =>{
 });
 
 const app = express();
+// Instead of ejs use ejsMate
+app.engine('ejs' , ejsMate);
 app.set('view engine' , 'ejs');
 app.set('views' , path.join(__dirname, 'views'))
 // now the req.body is parsed
--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i=0 ; i< 50 ; i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const randomPrice = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             location : `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            price: randomPrice
         })
         await camp.save();
 

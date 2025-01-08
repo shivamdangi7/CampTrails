86394c87343629e8409c5d3699d1cf9fa56f1204
shivamdangi7 <shivamdangi0111@gmail.com>
2025-01-08 19:19:43 +0530

    dbUrl change to mongo atlas

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const helmet = require('helmet');
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
-// const dbUrl = process.env.DB_URL;
-const dbUrl = 'mongodb://localhost:27017/camp-trails';
+const dbUrl = process.env.DB_URL;
+// const dbUrl = 'mongodb://localhost:27017/camp-trails';
 
 mongoose.connect(dbUrl)
 
